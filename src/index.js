dd145cf0404c927943a22d3edf9f6916fccaca96
Aileduk <kudelia1997@gmail.com>
2022-03-30 23:31:48 +0300

    revork

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
